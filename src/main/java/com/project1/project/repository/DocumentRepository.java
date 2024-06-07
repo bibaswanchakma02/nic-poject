c3bed840a4e4df1e1081df85302f00f85b106d60
Rupak100 <112826831+Rupak100@users.noreply.github.com>
2024-06-07 19:27:33 +0530

    getadocumentofaperson API

--- src/main/java/com/project1/project/repository/DocumentRepository.java
+++ src/main/java/com/project1/project/repository/DocumentRepository.java
@@ -3,9 +3,12 @@ package com.project1.project.repository;
 
 import com.project1.project.model.Document;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface DocumentRepository extends MongoRepository<Document, UUID> {
-
+    @Query("{ 'client_id': ?0 }")
+    Optional<Document> findByClientId(String client_id);
 }
